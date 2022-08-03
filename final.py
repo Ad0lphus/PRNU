1eda468a600adb21ff8ed5830ecf6cb62155d17a
Ad0lphus <prabith7.g.s@gmail.com>
2022-08-03 22:09:04 +0530

    Fingerprint and PRNU updation

--- final.py
+++ final.py
@@ -1,9 +1,6 @@
 # -*- coding: UTF-8 -*-
 """
-@author: Luca Bondi (luca.bondi@polimi.it)
-@author: Paolo Bestagini (paolo.bestagini@polimi.it)
-@author: Nicolò Bonettini (nicolo.bonettini@polimi.it)
-Politecnico di Milano 2018
+@author: Prabith GS
 """
 
 import os
